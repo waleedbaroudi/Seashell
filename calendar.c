4a4016df47c960a06f5bbadd7b82db7a71ae1002
AbdullrahmanHMD <manofsteel272@yahoo.com>
2021-04-01 01:25:03 +0300

    Implement 'list' command. Add assignEvent method. Add printEventInfo method

--- calendar.c
+++ calendar.c
@@ -4,6 +4,7 @@
 #include <limits.h>
 #include <unistd.h>
 
+#define STR_LIMIT 1000
 
 char *homedir; // home directory on the current machine (to place the shortdirs in)
 char schedsPath[PATH_MAX]; // file path of the text file stroing shortdirs
@@ -115,56 +116,135 @@ void schedule(Event *event) {
     // write to file
     FILE *schedsFile = fopen(schedsPath, "a");
     fprintf(schedsFile, "%s %s %s %s * XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send '%s' '%s'\n",
-         minute, 
-         hour, 
-         day, 
-         month, 
-         e.title, 
-         e.description); // write crontab job for the schedule
-
+	    minute, 
+	    hour, 
+	    day, 
+	    month, 
+	    e.title, 
+	    e.description); // write crontab job for the schedule
+    
     if (e.shouldRemind)
-    {
-        rewindTenMinutes(&minute, &hour, &day, &month);
+      {
+	rewindTenMinutes(&minute, &hour, &day, &month);
         fprintf(schedsFile, "%s %s %s %s * XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send 'Reminder: %s' '[IN 10 MINUTES] %s'\n",
-         minute, 
-         hour, 
-         day, 
-         month, 
+		minute, 
+		hour, 
+		day, 
+		month, 
          e.title, 
-         e.description); // write crontab job for the reminder
-    } else {
-        fprintf(schedsFile, "\n");
+		e.description); // write crontab job for the reminder
+      } else {
+      fprintf(schedsFile, "\n");
     }
     
     fclose(schedsFile);
-
+    
     printf("Scheduling...\n");
     
     char *argv[3];
-      argv[0] = "crontab";
-      argv[1] = schedsPath;
-      argv[2] = NULL;
-      execvp("crontab", argv);
+    argv[0] = "crontab";
+    argv[1] = schedsPath;
+    argv[2] = NULL;
+    execvp("crontab", argv);
 }
+
+/**
+ * Creates an Event struct instance with the information extracted from the given string.
+ */
+Event* assignEvent(char* line){
+  
+  char *substr = strstr(line,"'");
+  substr++;
+  
+  char *title = strtok(substr, "'");
+  strtok(NULL, "'");
+  char *desc = strtok(NULL, "'");
+  
+  char* timeSlots[5];
+  char* token = strtok(line, " ");
+    
+  for(int i = 0; i < 5; i++){
+    if(!strcmp(token, "*"))
+      timeSlots[i] = "all";
+    else 
+      timeSlots[i] = token;
+    token = strtok(NULL, " ");
+  }
+  
+  char* time;
+  
+  strcpy(time, timeSlots[1]);
+  strcat(time, ":");
+  
+  if(atoi(timeSlots[0]) < 10)
+    strcat(time, "0");
+  
+  strcat(time, timeSlots[0]);
+  
+  char* date;
+  
+  strcpy(date, timeSlots[2]);
+  strcat(date, "-");
+  
+  strcat(date, timeSlots[3]);
+  
+  return makeEvent(title, desc, time, date, "");
+  
+}
+
+void printEventInfo(Event e){
+  printf(" _________________________________________\n/\n");
+  printf("| Title: %s\n", e.title);
+  printf("| Description: \n| %s\n", e.description);
+  printf("|----------------------------------------+\n");
+  printf("| On: %s\tAt: %s\n", e.date, e.time);
+  printf("|----------------------------------------+\n");
+  printf("| Reminder: %s\n", e.shouldRemind? "enabled" : "not enabled");
+  printf("\\_________________________________________\n");
+}
+
 
 int main(int argc, char *argv[]) {
 
-    // set home and schedsdir paths
-    homedir = getenv("HOME");
-    strcat(homedir, "/Documents");
-    strcpy(schedsPath, homedir);
-    strcat(schedsPath, "/scheds.txt");
+      // set home and schedsdir paths
+      homedir = getenv("HOME");
+      strcat(homedir, "/Documents");
+      strcpy(schedsPath, homedir);
+      strcat(schedsPath, "/scheds.txt");
+      
+      char* option = argv[1];
+      
+      if (strcmp(option, "schedule") == 0)
+	{
+	  Event *event = makeEvent(argv[2], argv[3], argv[4], argv[5], argv[6]);
+	  schedule(event);
+	  return 0;
+	}
+      if (strcmp(option, "list") == 0)
+	{
+	  char line[STR_LIMIT];
+	  char line2[STR_LIMIT];
+	  FILE *schedsFile;
+	  schedsFile = fopen(schedsPath, "r");
+	  
+	  if(schedsFile == NULL){
+	    printf("Failed to open file.\n");
+	    return 1;
+	  }
+	  
+	  while(fgets(line, STR_LIMIT, schedsFile)){
+	    Event e = *assignEvent(line);
 
-    char* option = argv[1];
+	    fgets(line2, STR_LIMIT, schedsFile);
 
-    if (strcmp(option, "schedule") == 0)
-    {
-        Event *event = makeEvent(argv[2], argv[3], argv[4], argv[5], argv[6]);
-        schedule(event);
-        return 0;
+	    if(!strcmp(line2, "\n"))
+	      e.shouldRemind = 0;
+	    else
+	      e.shouldRemind = 1;
+	    printEventInfo(e);
+	    
+	  }
+	}
+      return 1;
     }
     
-
-
-    return 1;
-}
