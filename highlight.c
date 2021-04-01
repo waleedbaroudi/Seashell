9214b7d101fa629cac89b361836f220791a5eb20
AbdullrahmanHMD <manofsteel272@yahoo.com>
2021-04-02 01:18:05 +0300

    Fix highlight adding new line. Add documentation to highlight.c

--- highlight.c
+++ highlight.c
@@ -41,7 +41,12 @@ int main(int argc, char *argv[]){
   return 0;
 }
 
-
+/**
+ * Highlights a given word with a given color in a given string.
+ * @param  text   A string to highlight words in.
+ * @param  word   A word to highlight in the given string.
+ * @param  color  The color to highlight the given word with.       
+ */
 void colorWordsInLine(char* text, char* word, char* color){
   char delim[] = " ";
 
@@ -50,8 +55,7 @@ void colorWordsInLine(char* text, char* word, char* color){
   
   while(token != NULL){
     if(token[strlen(token) - 1] == '\n'){
-      strcat(lines, coloredToken(substring(token, strlen(token) - 1), word, color));  
-            strcat(lines, "\n");
+      strcat(lines, coloredToken(substring(token, strlen(token) - 1), word, color));    
     }
     else{
       strcat(lines, coloredToken(token, word, color));  
@@ -63,6 +67,15 @@ void colorWordsInLine(char* text, char* word, char* color){
   printf("%s\n", lines);
 }
 
+/**
+ * Given two strings word and compateTo, colors compateTo if word and compareTo match and return compareTo.
+ * return word otherwise.
+ * @param  word          A string to compare with the other string.
+ * @param  compareTo     A string to compare with the other stirng.
+ * @param  color         A string representing a color to color the second string if both strings match.
+ * @return               Returns a string identical to the second given string but colored with the given color if 
+ *                       the two given strings match, and the first string otherwise.
+ */
 char* coloredToken(char* word, char* compareTo, char* color){
     char temp[strlen(compareTo) + 1];
 
@@ -87,6 +100,12 @@ char* coloredToken(char* word, char* compareTo, char* color){
     return word;
 }
 
+/**
+ * Given a character representing a color, returns a string that changes
+ * the color of a string if placed before it.
+ * @param  word   A string to representing a color (red, green, blue).
+ * @return        returns a string that changes the color of a string if placed before it.         
+ */
 char* getColor(char* color){
     if(!strcmp(color, "r")){
         return COLOR_RED;
@@ -100,6 +119,12 @@ char* getColor(char* color){
     return NULL;
 }
 
+/**
+ * Given a string and a color, returns a string colored with the given color.
+ * @param  word   A string to color.
+ * @param  color  A string describing a color.
+ * @return        Returns a colored string according to the parameters.
+ */
 char* getColoredWord(char* word, char* color){
     char* c = getColor(color);
     char str[100] = "";
